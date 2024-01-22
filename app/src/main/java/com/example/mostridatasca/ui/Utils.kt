71ea3e7174fa9ca90fedd21733d9b93edbdc26c7
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-01-22 07:27:04 +0100

    aggiunte label alla bottom navigation

--- app/src/main/java/com/example/mostridatasca/ui/Utils.kt
+++ app/src/main/java/com/example/mostridatasca/ui/Utils.kt
@@ -3,36 +3,41 @@ package com.example.mostridatasca.ui
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mostridatasca.R
-import com.example.mostridatasca.ui.theme.MostriDaTascaTheme
 
 
 @Preview
 @Composable
-fun MyNavigationBar() {
-    NavigationBar {
+fun MyNavigationBar(modifier: Modifier = Modifier) {
+    NavigationBar(modifier = modifier) {
         NavigationBarItem(
             selected = true,
             onClick = { /*TODO*/ },
-            icon = { Icon(painterResource(id = R.drawable.map), contentDescription = null) }
+            icon = { Icon(painterResource(id = R.drawable.map), contentDescription = null) },
+            label = { Text("Map") }
         )
         NavigationBarItem(
             selected = true,
             onClick = { /*TODO*/ },
-            icon = { Icon(painterResource(id = R.drawable.view_list), contentDescription = null) }
+            icon = { Icon(painterResource(id = R.drawable.view_list), contentDescription = null) },
+            label = { Text("Objects") }
         )
         NavigationBarItem(
             selected = true,
             onClick = { /*TODO*/ },
-            icon = { Icon(painterResource(id = R.drawable.trophy), contentDescription = null) }
+            icon = { Icon(painterResource(id = R.drawable.trophy), contentDescription = null) },
+            label = { Text("Leaderboard") }
         )
         NavigationBarItem(
             selected = true,
             onClick = { /*TODO*/ },
-            icon = { Icon(painterResource(id = R.drawable.account), contentDescription = null) }
+            icon = { Icon(painterResource(id = R.drawable.account), contentDescription = null) },
+            label = { Text("Profile") }
         )
     }
 }
