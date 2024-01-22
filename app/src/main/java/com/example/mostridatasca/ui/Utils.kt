a2945152c63ee71bc0baef968c7a5f44236fc3af
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-01-22 09:49:20 +0100

    ListItem per object

--- app/src/main/java/com/example/mostridatasca/ui/Utils.kt
+++ app/src/main/java/com/example/mostridatasca/ui/Utils.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mostridatasca.R
@@ -47,9 +48,14 @@ fun MyNavigationBar(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ImageFromBase64(image: String, modifier: Modifier = Modifier) {
+fun ImageFromBase64(image: String, modifier: Modifier = Modifier, contentScale: ContentScale = ContentScale.Fit) {
     val byteArray = Base64.decode(image, Base64.DEFAULT);
     val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
 
-    Image(bitmap = bitmap.asImageBitmap(), contentDescription = null, modifier = modifier)
+    Image(
+        bitmap = bitmap.asImageBitmap(),
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = contentScale
+    )
 }
