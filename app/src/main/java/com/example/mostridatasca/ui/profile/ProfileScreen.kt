a72c4a7e5f6197622d782c2f5931223cfccf1f77
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-01-22 11:26:38 +0100

    UserInformation

--- app/src/main/java/com/example/mostridatasca/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/mostridatasca/ui/profile/ProfileScreen.kt
@@ -4,9 +4,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,7 +49,61 @@ fun UserImage(
     }
 }
 
+@Composable
+fun UserInformation(
+    name: String,
+    lifePoints: String,
+    experience: String
+) {
+    Column {
+        ListItem(
+            leadingContent = { Icon(Icons.Default.Face, contentDescription = null) },
+            headlineContent = {
+                TextField(
+                    value = name,
+                    onValueChange = { TODO() }
+                )
+            },
+            trailingContent = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Default.Edit, contentDescription = null)
+                }
+            }
+        )
+        ListItem(
+            leadingContent = { Icon(Icons.Default.Place, contentDescription = null) },
+            headlineContent = { Text("Position share") },
+            trailingContent = { Switch(checked = true, onCheckedChange = { TODO() }) }
+        )
+        Divider()
+        ListItem(
+            leadingContent = { Icon(Icons.Default.Favorite, contentDescription = null) },
+            headlineContent = { Text("Life points") },
+            trailingContent = {
+                SuggestionChip(
+                    onClick = { /*TODO*/ },
+                    label = { Text(lifePoints) })
+            }
+        )
+        ListItem(
+            leadingContent = { Icon(Icons.Default.Star, contentDescription = null) },
+            headlineContent = { Text("Experience") },
+            trailingContent = {
+                SuggestionChip(
+                    onClick = { /*TODO*/ },
+                    label = { Text(experience) })
+            }
+        )
+        Divider()
+    }
+}
+
 @Preview
 @Composable
 fun MyPreview() {
+    UserInformation(
+        name = "Pablo",
+        lifePoints = "100",
+        experience = "50"
+    )
 }
