c3cea146c5997da2a95b99344b9d9e538d21ac7f
pablo-aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-02-03 17:44:22 +0100

    UserScreen mostra mappa con posizione utente per gli utenti che hanno abilitato la posizione

--- app/src/main/java/com/example/mostridatasca/ui/leaderboard/UserScreen.kt
+++ app/src/main/java/com/example/mostridatasca/ui/leaderboard/UserScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -27,6 +28,14 @@ import com.example.mostridatasca.R
 import com.example.mostridatasca.model.User
 import com.example.mostridatasca.ui.ImageFromBase64
 import com.example.mostridatasca.ui.profile.UserInformation2
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 
 @Composable
 fun UserScreen(
@@ -73,5 +82,25 @@ fun UserScreen(
             experience = user?.experience.toString()
         )
         Divider()
+        if (user?.positionshare!!) {
+            GoogleMap(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                properties = MapProperties(
+                    minZoomPreference = 12.0f
+                ),
+                cameraPositionState = CameraPositionState(
+                    position = CameraPosition(LatLng(user.lat!!, user.lon!!), 0.0f, 0.0f, 0.0f)
+                )
+            ) {
+                Marker(
+                    state = MarkerState(
+                        position = LatLng(user.lat, user.lon)
+                    ),
+                    icon = BitmapDescriptorFactory.fromResource(R.drawable.player)
+                )
+            }
+        }
     }
 }
